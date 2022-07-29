9985539cf4c224ee8af574abb5cb18d9db5cf75c
BuiTheBoi <uNUKEcorn15@gmail.com>
2022-07-29 15:29:14 -0700

    Completed project

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # AI Chatbot
+A mini AI Chatbot project named AndyBot, implemented to demonstrate applications on Deep Learning, specifically
+Natural Language Processing. This project was done from a YouTube tutorial from [NeuralNine](https://www.youtube.com/watch?v=1lwddP0KUEg)
 
-Video Tutorial: https://www.youtube.com/watch?v=1lwddP0KUEg
-
-Virtual environments: https://www.youtube.com/watch?v=ThU13tikHQw
-
-VS Code How to Solve Error: https://www.youtube.com/watch?v=GqTsFOtZiQI
+## Tech Stack:
+<p align="left">
+<img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54" alt="html" style="vertical-align:top; margin:3px">
+<img src="https://img.shields.io/badge/TensorFlow-FF6F00?style=for-the-badge&logo=TensorFlow&logoColor=white" alt="html" style="vertical-align:top; margin:3px">
+<img src="https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white" alt="html" style="vertical-align:top; margin:3px">
+</p>
 
 ## Virtual Environments
 To create new virtual environment:
@@ -21,3 +24,11 @@ To activate virtual environment (Note: First command is if you have permission i
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 venv\Scripts\Activate.ps1
 ```
+
+
+## Resources
+Video Tutorial: https://www.youtube.com/watch?v=1lwddP0KUEg
+
+Virtual environments: https://www.youtube.com/watch?v=ThU13tikHQw
+
+VS Code How to Solve Error: https://www.youtube.com/watch?v=GqTsFOtZiQI
--- training.py
+++ training.py
@@ -7,7 +7,7 @@ import json
 import random
 
 # Tensorflow imports
-# from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.models import Sequential
 
@@ -38,6 +38,51 @@ for intent in intents['intents']:
 # Eliminating any useless punctuation
 words = [lemmatizer.lemmatize(word)
          for word in words if word not in ignore_letters]
-words = sorted(set(words))  # Gets rid of duplicates and sorts in order
 
-print(words)
+# Gets rid of duplicates and sorts in order
+words = sorted(set(words))
+classes = sorted(set(classes))
+
+# Writing binaries pickle
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+# ---------------------- Creating bag of words ----------------------
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]  # Accessing the word list
+    word_patterns = [lemmatizer.lemmatize(
+        word.lower()) for word in word_patterns]
+
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])  # features
+train_y = list(training[:, 1])  # labels
+
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y),
+                 epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+print('Success!')
